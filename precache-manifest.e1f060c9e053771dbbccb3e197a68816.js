d4441247e4692dea223dfa891ef4f179fc40c8e7
adam-macgregor <43255693+adam-macgregor@users.noreply.github.com>
2020-06-30 12:17:17 -0500

    Updates

---
+++ precache-manifest.e1f060c9e053771dbbccb3e197a68816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8466604082b7769ef6337d4cb21c1444",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "a89cb1132eb750473908",
+    "url": "/react-todolist/static/css/main.a924be31.chunk.css"
+  },
+  {
+    "revision": "bb13a47ba1898596eff4",
+    "url": "/react-todolist/static/js/2.52a120b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.52a120b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89cb1132eb750473908",
+    "url": "/react-todolist/static/js/main.03a01ec1.chunk.js"
+  },
+  {
+    "revision": "dea162c310a378ff9d0d",
+    "url": "/react-todolist/static/js/runtime-main.82f4965a.js"
+  }
+]);
